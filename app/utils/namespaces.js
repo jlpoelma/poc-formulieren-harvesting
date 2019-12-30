e0c59bf704fb0c7ea2b5d4006e633fedf425e7fc
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-12-30 15:58:22 +0100

    fix SKOS export (with madnificent)

--- app/utils/namespaces.js
+++ app/utils/namespaces.js
@@ -3,5 +3,6 @@ import rdflib from './rdflib';
 const RDF = new rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 const FORM = new rdflib.Namespace("http://lblod.data.gift/vocabularies/forms/");
 const SHACL = new rdflib.Namespace("http://www.w3.org/ns/shacl#");
+const SKOS = new rdflib.Namespace("http://www.w3.org/2004/02/skos/core#");
 
-export { RDF, FORM, SHACL };
+export { RDF, FORM, SHACL, SKOS };
