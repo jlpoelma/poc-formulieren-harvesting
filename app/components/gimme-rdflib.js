4aeeeb372851443d0dd42ba041d5bb4a6dab38b1
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-12-30 16:00:05 +0100

    execute the validations (with madnificent)

--- app/components/gimme-rdflib.js
+++ app/components/gimme-rdflib.js
@@ -4,13 +4,20 @@ import Component from '@glimmer/component';
 import rdflib from '../utils/rdflib';
 import dilbeek from '../utils/dilbeek';
 import form from '../utils/besluitenlijst-formulier';
+import documentTypeCodelist from '../utils/codelist/document-type';
 import importTriplesForForm from '../utils/import-triples-for-form';
 import { triplesForPath, fieldsForForm } from '../utils/import-triples-for-form';
 import { RDF, FORM, SHACL } from '../utils/namespaces';
+import constraintForUri from '../utils/constraints';
 
 const dedup = function(arr){
   return [...new Set(arr)];
 };
+
+const FORM_GRAPH = new rdflib.NamedNode("http://mu.semte.ch/form");
+const SOURCE_GRAPH = new rdflib.NamedNode("http://mu.semte.ch/dilbeek");
+const SOURCE_NODE = new rdflib.NamedNode("http://mu.semte.ch/vocabularies/ext/besluitenlijsten/208ee6e0-28b1-11ea-972c-8915ff690069");
+const META_GRAPH = new rdflib.NamedNode("http://mu.semte.ch/metagraph");
 
 export default class GimmeRdflibComponent extends Component {
   store = null;
@@ -33,6 +40,9 @@ export default class GimmeRdflibComponent extends Component {
   @tracked
   formFieldsData = [];
 
+  @tracked
+  validatedFormFieldsData = [];
+
   get largeTable(){
     return !this.smallTable;
   }
@@ -44,8 +54,8 @@ export default class GimmeRdflibComponent extends Component {
 
   get graphGroupedTriples(){
     let graphs = this
-          .statements
-          .map( ({graph}) => graph.value );
+      .statements
+      .map( ({graph}) => graph.value );
     graphs = dedup(graphs);
 
     const response = graphs.map( (graph) => {
@@ -66,6 +76,7 @@ export default class GimmeRdflibComponent extends Component {
     // this.store.clear();
     rdflib.parse( dilbeek, this.store, "http://mu.semte.ch/dilbeek", "text/html" );
     rdflib.parse( form, this.store, "http://mu.semte.ch/form", "text/turtle" );
+    rdflib.parse( documentTypeCodelist, this.store, "http://mu.semte.ch/metagraph", "text/turtle" );
 
     this.statements = this.store.statements;
 
@@ -116,24 +127,82 @@ export default class GimmeRdflibComponent extends Component {
 
   @action
   renderForms(){
-    const FORM_GRAPH = new rdflib.NamedNode("http://mu.semte.ch/form");
-    const SOURCE_GRAPH = new rdflib.NamedNode("http://mu.semte.ch/dilbeek");
-    const SOURCE_NODE = new rdflib.NamedNode("http://mu.semte.ch/vocabularies/ext/besluitenlijsten/208ee6e0-28b1-11ea-972c-8915ff690069");
 
     let fieldsUri = fieldsForForm({
       store: this.store,
       formGraph: FORM_GRAPH
     });
 
-    this.formFieldsData = fieldsUri.map( uri => {
-      let path = this.store.any( uri, SHACL("path"), undefined, FORM_GRAPH);
-      let displayType = this.store.any( uri, FORM("displayType"), undefined, FORM_GRAPH);
+    this.formFieldsData = fieldsUri.map(this.generateFormFieldData.bind(this));
+  }
 
-      let values = triplesForPath({
-        store: this.store, path, formGraph: FORM_GRAPH, sourceNode: SOURCE_NODE, sourceGraph: SOURCE_GRAPH
-      }).values;
+  generateFormFieldData(uri){
+    let path = this.store.any( uri, SHACL("path"), undefined, FORM_GRAPH);
+    let displayType = this.store.any( uri, FORM("displayType"), undefined, FORM_GRAPH);
 
-      return { displayType, values };
+    let values = triplesForPath({
+      store: this.store, path, formGraph: FORM_GRAPH, sourceNode: SOURCE_NODE, sourceGraph: SOURCE_GRAPH
+    }).values;
+
+    return { uri, displayType, values };
+
+  }
+
+  @action
+  validateForms(){
+
+    let fieldsUri = fieldsForForm({
+      store: this.store,
+      formGraph: FORM_GRAPH
     });
+
+    let formFieldsData = [];
+
+    for(let uri of fieldsUri){
+      let path = this.store.any( uri, SHACL("path"), undefined, FORM_GRAPH);
+
+      let validationConstraintUris = this.store
+                                         .match( uri, FORM("validations"), undefined, FORM_GRAPH)
+                                         .map(t => t.object);
+
+      let validationResults = [];
+
+      for(let constraintUri of validationConstraintUris){
+        //detect type
+        const validationType = this.store.any(constraintUri, RDF('type'), undefined, FORM_GRAPH);
+        const groupingType = this.store.any(constraintUri, FORM("grouping"), undefined, FORM_GRAPH).value;
+
+        let validator = constraintForUri(validationType && validationType.value);
+        if( !validator ) continue;
+
+        let values = triplesForPath({
+          store: this.store, path, formGraph: FORM_GRAPH, sourceNode: SOURCE_NODE, sourceGraph: SOURCE_GRAPH
+        }).values;
+
+        const options = { store: this.store, metaGraph: META_GRAPH, constraintUri: constraintUri };
+
+        let validationResult;
+
+        if( groupingType == FORM("Bag").value ) {
+          validationResult = validator( values, options );
+        } else if( groupingType == FORM("AnyMatch").value ) {
+          validationResult = values.some( (value) => validator( value, options ) );
+        } else if( groupingType == FORM("AllMatch" .value) ) {
+          validationResult = values.every( (value) => validator( value, options ) );
+        }
+
+        validationResults.push(validationResult);
+      }
+
+      console.log({validationResults});
+
+      const formData = this.generateFormFieldData(uri);
+      formData.validationsResult = validationResults;
+      formFieldsData.push(formData);
+
+    };
+
+    this.validatedFormFieldsData = formFieldsData;
+
   }
 }
